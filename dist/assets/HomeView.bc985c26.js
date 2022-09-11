b5a946d697e7b58032581261be9b3a22d9ac187f
caioqf <caio8200@gmail.com>
2022-09-11 14:36:18 -0300

    add dist

---
+++ dist/assets/HomeView.bc985c26.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./Title.956fd200.js";import{o,c as a,a as s}from"./index.ab98975a.js";const t={class:"homepage"},p={__name:"HomeView",setup(c){return(_,r)=>(o(),a("div",t,[s(e)]))}};export{p as default};
