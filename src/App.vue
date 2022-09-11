95cc65f2b9473f63f914a4ec6d967f6742730938
caioqf <caio8200@gmail.com>
2022-09-11 00:07:21 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Nav from "./components/Nav.vue";
+</script>
+
+<template>
+  <header>
+    <Nav />
+  </header>
+  <main>
+    <div class="content"></div>
+  </main>
+</template>
+
+<style scoped>
+header {
+  width: 35%;
+}
+main {
+  display: flex;
+  align-items: center;
+}
+
+.content {
+  margin: 5vh;
+  width: 120vh;
+  height: 90vh;
+  border: 1px solid white;
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav class="navigation">
+    <div class="logo">
+      <img src="../assets/logo.png" />
+    </div>
+    <div class="nav-options">
+      <div>Home</div>
+      <div>About</div>
+      <div>Skills</div>
+      <div>Projects</div>
+      <div>Contact</div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+</script>
+<style scoped>
+.navigation {
+  display: flex;
+  align-items: center;
+  height: 100vh;
+  margin: 0px;
+}
+
+.nav-options :hover {
+  cursor: pointer;
+  color: rgb(127, 206, 17);
+  text-decoration: underline;
+}
+
+.logo > img {
+  width: 80%;
+  margin: 2em;
+  border: 1px solid white;
+}
+
+.logo > img:hover {
+  border: 1px solid rgb(127, 206, 17);
+  cursor: pointer;
+}
+</style>
